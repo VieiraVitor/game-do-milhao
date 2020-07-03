e34e549b6c571c068612b03732dc0d0bbe9011e0
VieiraVitor <vieirachp@hotmail.com>
2020-07-03 19:26:01 -0300

    feat: recebendo resposta selecionada e colocando estilo

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@ let databaseQuestions = [
         alternative2: 'aquilo',
         alternative3: 'nao sei',
         alternative4: 'nada',
-        answer: 'nada'
+        answer: 4
     },
 
     {
@@ -14,7 +14,7 @@ let databaseQuestions = [
         alternative2: 'aquilo',
         alternative3: 'nao sei',
         alternative4: 'nada',
-        answer: 'nada'
+        answer: 2
     },
     {
         question: 'Pergunta 3 ?',
@@ -22,7 +22,7 @@ let databaseQuestions = [
         alternative2: 'aquilo',
         alternative3: 'nao sei',
         alternative4: 'nada',
-        answer: 'nada'
+        answer: 2
     },
     {
         question: 'Pergunta 4 ?',
@@ -30,16 +30,24 @@ let databaseQuestions = [
         alternative2: 'aquilo',
         alternative3: 'nao sei',
         alternative4: 'nada',
-        answer: 'nada'
+        answer: 1
     },
 
 ]
 
 const maxQuestions = 16
 let drawnQuestions = 0
+let questionSelected = {}
 
 const question = document.querySelector('#question')
-const alternatives = document.querySelectorAll('#answer')
+const alternatives = document.querySelectorAll('.answer')
+const btnClicked = document.querySelector('#answer1')
+
+
+
+selectedChoice = () => {
+    btnClicked.addEventListener('click', () => btnClicked.style.backgroundColor = '#fff')
+}
 
 questionsAvailable = []
 questionsAvailable = databaseQuestions
@@ -53,10 +61,12 @@ getNewQuestion = () => {
 
     //sortear uma questao pelo índice
     const drawnQuestion = Math.floor(Math.random() * questionsAvailable.length)
-    question.innerHTML = questionsAvailable[drawnQuestion].question
+    questionSelected = questionsAvailable[drawnQuestion]
+    question.innerHTML = questionSelected.question
 
-    alternatives.forEach((alternative, index) => {
-        alternative.innerHTML = questionsAvailable[drawnQuestion]['alternative' + (index + 1)]
+    alternatives.forEach(alternative => {
+        const alternativeNumber = alternative.dataset['id']
+        alternative.innerHTML = questionSelected['alternative' + alternativeNumber]
     })
     
     drawnQuestions++
@@ -65,3 +75,18 @@ getNewQuestion = () => {
 }
 
 getNewQuestion()
+
+alternatives.forEach( alternative => {
+    alternative.addEventListener('click', e => {
+        
+        const alternativeSelected = e.target.dataset['id']
+
+        e.target.classList.add('answer-selected')
+        
+        let classToApply = alternativeSelected == questionSelected.answer ? 'answer-corret' : 'answer-incorrect'
+
+        // se confirmar a resposta
+            .target.classList.add(classToApply)
+
+    }
+)})
